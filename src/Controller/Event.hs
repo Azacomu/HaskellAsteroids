fedf206fdf4bd4e1383240ad3a6ccc6b50820ba1
Martin Boers <martin-003@live.nl>
2016-10-26 14:55:26 +0200

    Lenses are set up
    Changed initial model to use lenses in the datatype, eventhandler now
    uses lenses correctly as well.

--- src/Controller/Event.hs
+++ src/Controller/Event.hs
@@ -2,6 +2,7 @@ module Controller.Event (
     eventHandler
 ) where
 
+import Control.Lens
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 
@@ -10,21 +11,21 @@ import Model
 -- | Event handling
 
 eventHandler :: Event -> World -> World
-eventHandler (EventKey (SpecialKey KeyLeft ) Down _ _) world
-    = world { rotateAction = RotateLeft }
-eventHandler (EventKey (SpecialKey KeyLeft ) Up   _ _) world
-    = world { rotateAction = NoRotation }
-eventHandler (EventKey (SpecialKey KeyRight) Down _ _) world
-    = world { rotateAction = RotateRight }
-eventHandler (EventKey (SpecialKey KeyRight) Up   _ _) world
-    = world { rotateAction = NoRotation }
-eventHandler (EventKey (SpecialKey KeyUp   ) Down _ _) world
-    = world { movementAction = Thrust }
-eventHandler (EventKey (SpecialKey KeyUp   ) Up   _ _) world
-    = world { movementAction = NoMovement }
-eventHandler (EventKey (SpecialKey KeySpace) Down _ _) world
-    = world { shootAction = Shoot }
-eventHandler (EventKey (SpecialKey KeySpace) Up   _ _) world
-    = world { shootAction = DontShoot }
-eventHandler _ world
-    = world
+eventHandler (EventKey (SpecialKey KeyLeft ) Down _ _)
+    = rotateAction .~ RotateLeft
+eventHandler (EventKey (SpecialKey KeyLeft ) Up   _ _)
+    = rotateAction .~ NoRotation
+eventHandler (EventKey (SpecialKey KeyRight) Down _ _)
+    = rotateAction .~ RotateRight
+eventHandler (EventKey (SpecialKey KeyRight) Up   _ _)
+    = rotateAction .~ NoRotation
+eventHandler (EventKey (SpecialKey KeyUp   ) Down _ _)
+    = movementAction .~ Thrust
+eventHandler (EventKey (SpecialKey KeyUp   ) Up   _ _)
+    = movementAction .~ NoMovement
+eventHandler (EventKey (SpecialKey KeySpace) Down _ _)
+    = shootAction .~ Shoot
+eventHandler (EventKey (SpecialKey KeySpace) Up   _ _)
+    = shootAction .~ DontShoot
+eventHandler _
+    = id
--- src/Model.hs
+++ src/Model.hs
@@ -1,24 +1,41 @@
-{-# LANGUAGE DisambiguateRecordFields, NamedFieldPuns #-}
+{-# LANGUAGE TemplateHaskell, DisambiguateRecordFields, NamedFieldPuns #-}
 
 module Model where
 
+import Control.Lens
 import System.Random
 
 -- | Game state
 
+--Basic data of World, add other datatypes used below
 data World = World {
         -- Random generator
-        rndGen           :: StdGen,
+        _rndGen           :: StdGen,
         -- Event queue
-        rotateAction     :: RotateAction,
-        movementAction   :: MovementAction,
-        shootAction      :: ShootAction
+        _rotateAction     :: RotateAction,
+        _movementAction   :: MovementAction,
+        _shootAction      :: ShootAction
         -- TODO: add more fields here!
-    }
+    }deriving (Show)
     
-data RotateAction   = NoRotation | RotateLeft | RotateRight
-data MovementAction = NoMovement | Thrust
-data ShootAction    = Shoot      | DontShoot
+data RotateAction   = NoRotation | RotateLeft | RotateRight deriving (Show)
+data MovementAction = NoMovement | Thrust                   deriving (Show)
+data ShootAction    = Shoot      | DontShoot                deriving (Show)
 
+--TODO: Add more datatypes here (player/enemy/asteroid)
+data Player = Player {_position :: Point
+                     } deriving (Show)
+data Point = Point {_x :: Double,
+                    _y :: Double
+                    } deriving (Show)
+
+--Add lenses below (must be after defining datatypes)
+--(TemplateHaskell can do this automatically with makeLenses,
+-- this will make a lens for all _ vars in the datatype )
+makeLenses ''World
+makeLenses ''Player
+makeLenses ''Point
+
+--Returns the starting world of the game based on given seed
 initial :: Int -> World
 initial seed = World {}
